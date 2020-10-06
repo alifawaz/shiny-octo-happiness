3adf5c34d9cadd899c3d99b12b68facb52351127
alifawaz <42553270+alifawaz@users.noreply.github.com>
2020-10-05 18:21:50 -0700

    Add files via upload

---
+++ AddingMachine.java
@@ -0,0 +1,36 @@
+// Ali Aldulaimi
+// cse 360 HW 1
+package cse360assignment02;
+
+public class AddingMachine {
+    private int total;
+    private String history;
+
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+        history = "0";
+    }
+
+    public int getTotal () {
+        return total;
+    }
+
+    public void add (int value) {
+        total += value;
+        history += " + " + value;
+    }
+
+    public void subtract (int value) {
+        total -= value;
+        history += " - " + value;
+    }
+
+    public String toString () {
+        return history;
+    }
+
+    public void clear() {
+        this.total=0;
+
+    }
+}
